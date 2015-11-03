0b538796e49d08b57a01b0faca31831f93b98908
Joel Self <joelself@gmail.com>
2015-11-03 03:31:53 -0700

    Fixed TrainingData to use jagged arrays.

--- VS2010/CSharpXorTest/XorTest.cs
+++ VS2010/CSharpXorTest/XorTest.cs
@@ -31,19 +31,19 @@ namespace CSharpXorTest
                         Console.WriteLine("Error reading training data --- ABORTING.\n");
                         return -1;
                     }
-                    float[,] inputs = data.GetInput();
-                    float[,] outputs = data.GetOutput();
+                    float[][] inputs = data.GetInput();
+                    float[][] outputs = data.GetOutput();
                     for (int i = 0; i < data.LengthTrainData(); i++)
                     {
                         net.ResetMSE();
-                        float[] calc_out = net.Test((float[])inputs.GetValue(i), (float[])inputs.GetValue(i));
+                        float[] calc_out = net.Test(inputs[i], inputs[i]);
 
                         Console.WriteLine("XOR test ({0}, {1}) -> {2}, should be {3}, difference={4}",
-                            inputs[i, 0],
-                            inputs[i, 1],
+                            inputs[i][0],
+                            inputs[i][1],
                             calc_out[0],
-                            outputs[i, 0],
-                            calc_out[0] - outputs[i, 0]);
+                            outputs[i][0],
+                            calc_out[0] - outputs[i][0]);
                     }
                 }
             }
--- VS2010/FannWrapper/TrainingData.cs
+++ VS2010/FannWrapper/TrainingData.cs
@@ -22,40 +22,42 @@ namespace FannWrapper
             return InternalData.read_train_from_file(filename);
         }
 
-        public float[,] GetOutput()
+        public float[][] GetOutput()
         {
             using (floatArrayArray output = floatArrayArray.frompointer(InternalData.get_output()))
             {
                 int length = (int)InternalData.length_train_data();
                 int count = (int)InternalData.num_output_train_data();
-                float[,] result = new float[length, count];
+                float[][] result = new float[length][];
                 for (int i = 0; i < length; i++)
                 {
+                    result[i] = new float[count];
                     using (floatArray inputArray = floatArray.frompointer(output.getitem(i)))
                     {
                         for (int j = 0; j < count; j++)
                         {
-                            result[i, j] = inputArray.getitem(j);
+                            result[i][j] = inputArray.getitem(j);
                         }
                     }
                 }
                 return result;
             }
         }
-        public float[,] GetInput()
+        public float[][] GetInput()
         {
             using (floatArrayArray input = floatArrayArray.frompointer(InternalData.get_input()))
             {
                 int length = (int)InternalData.length_train_data();
                 int count = (int)InternalData.num_input_train_data();
-                float[,] result = new float[length, count];
+                float[][] result = new float[length][];
                 for (int i = 0; i < length; i++)
                 {
+                    result[i] = new float[count];
                     using (floatArray inputArray = floatArray.frompointer(input.getitem(i)))
                     {
                         for (int j = 0; j < count; j++)
                         {
-                            result[i,j] = inputArray.getitem(j);
+                            result[i][j] = inputArray.getitem(j);
                         }
                     }
                 }
