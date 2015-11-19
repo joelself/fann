89b285f7ca5b31c88810102ac569c45711d551b9
Joel Self <joelself@gmail.com>
2015-11-19 16:40:04 -0700

    Retargetted the dll projects for .Net 2.0 for better compatibility. Added IAccessor<T> interface to replace the IReadOnlyList<T> interface in .NET 4.5

---
+++ VS2010/FANNCSharp/FannWrapper/IAccessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FANNCSharp
+{
+    interface IAccessor<T> : IEnumerable<T>, IEnumerable
+    {
+        int Count
+        {
+            get;
+        }
+
+        T this[int index]
+        {
+            get;
+        }
+    };
+}
