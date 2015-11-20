d05231962283170e6fe22d410d9f7a1249082875
Joel <joelself@gmail.com>
2015-11-19 19:16:05 -0700

    Added documentation to IAccessor<T>

--- VS2010/FANNCSharp/FannWrapper/IAccessor.cs
+++ VS2010/FANNCSharp/FannWrapper/IAccessor.cs
@@ -4,14 +4,27 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace FANNCSharp
-{
+{   /* Interface: IAccessor<T>
+         An enumerable interface that provides a count of
+         elements in the accessor and random, read-only
+         access to its elements.
+    */
     interface IAccessor<T> : IEnumerable<T>, IEnumerable
     {
+        /* Property: Count
+             The number of elements in the accessor.
+        */
         int Count
         {
             get;
         }
+        /* Property: Item
+             Provides access to the element at index
 
+             Parameters:
+               index - the index of the item to retrieve
+             
+        */
         T this[int index]
         {
             get;
